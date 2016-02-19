338d4ccf1943638278a01acef3eee9b07f04cda9
Peter Lollo <peteralollo@gmail.com>
2016-02-19 12:29:20 -0800

    Add input form

--- foodZen/client/app/home/home.js
+++ foodZen/client/app/home/home.js
@@ -1,4 +1,19 @@
 angular.module('foodZen.home', [])
-.controller('HomeController', function(){
+.controller('HomeController', function($scope){
+
+  $scope.hitEnter = function($event) {
+    if($event.which === 13) {
+      console.log('enter button!');
+    }
+  };
+
+  $scope.addIngredient = function() {
+    console.log('ingredient', $scope.ingredient);
+    $scope.ingredient = '';
+  };
+
+  $scope.getRecipes = function() {
+    console.log('get request for recipes');
+  };
   console.log('home controller so running');
 });
