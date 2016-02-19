d957bd7be344dab045ac4040f66aba8be0c54914
Hack Reactor Students <accounts@hackreactor.com>
2016-02-19 12:34:15 -0800

    (WIP) Set up server-side folders and files

---
+++ foodZen/server/config/routes.js
@@ -0,0 +1,17 @@
+var ingredientController = require('../ingredients/ingredientController.js');
+var recipeController = require('../recipes/recipeController.js');
+
+var helpers = require('./helpers.js');
+
+module.exports = function (app, express) {
+
+  app.get('/api/recipes/', recipeController);
+  app.post('/api/ingredients/', ingredientController);
+
+
+
+  // If a request is sent somewhere other than the routes above,
+  // send it through our custom error handler
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+};
---
+++ foodZen/server/ingredients/ingredientController.js
@@ -0,0 +1,1 @@
+ingredientsController.js
