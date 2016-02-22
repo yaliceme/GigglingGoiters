f8c349a0046aa6e749834a5f747b34e1a5eaa546
Vincent Pham <vincentvpham@gmail.com>
2016-02-22 12:34:23 -0800

    defined tests for ingredient model

--- foodZen/server/tests/ingredientModelTest.js
+++ foodZen/server/tests/ingredientModelTest.js
@@ -1,1 +1,15 @@
-ingredientModelTest.js
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var Ingredient = require('../ingredients/ingredientModel.js');
+
+describe('Ingredient Model', function () {
+
+  it('Ingredient should be a Mongoose model', function () {
+    expect(new Ingredient()).to.be.instanceOf(mongoose.Model);
+  });
+
+  it('should have a schema', function () {
+    expect(Ingredient.schema).to.exist;
+  });
+
+});
