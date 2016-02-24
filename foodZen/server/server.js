35e3c2f35e41519b04b71b5376e29dfe94734de1
Alice Yu <yaliceme@gmail.com>
2016-02-24 11:39:13 -0800

    Change server port to be process.env.PORT || 3000

--- foodZen/server/server.js
+++ foodZen/server/server.js
@@ -8,8 +8,10 @@ mongoose.connect('mongodb://localhost/foodZen');
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-// start listening to requests on port 3000
-app.listen(3000);
-console.log("listening on port 3000");
+// start listening to requests on process.env.PORT or 3000
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log("listening on port " + port);
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
