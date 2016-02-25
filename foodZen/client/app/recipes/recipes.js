df034a421f9b2a4d635bb2e4f8a7da3a8e13699f
Peter Lollo <peteralollo@gmail.com>
2016-02-24 16:22:26 -0800

    Implement client-side get request for recipe instructions

--- foodZen/client/app/recipes/recipes.js
+++ foodZen/client/app/recipes/recipes.js
@@ -1,6 +1,10 @@
 angular.module('foodZen.recipes', [])
-.controller('RecipeController', function($scope, Recipes, Ingredients){
+.controller('RecipeController', function($scope, $http, Recipes, Ingredients){
   $scope.data = {};
+  $scope.singleView = {
+    view: false,
+    recipe: undefined
+  };
 
   var initializeRecipes = function(){
     Recipes.updateRecipes(Ingredients.ingredients)
@@ -10,5 +14,15 @@ angular.module('foodZen.recipes', [])
     });
   };
 
+  $scope.viewRecipe = function(id){
+    return $http({
+      method: 'GET',
+      url: '/api/recipes/ingredients/',
+      data: {id: id}
+    }).then(function( recipe ){
+      $scope.singleRecipe.view = true;
+      $scope.singleRecipe.recipe = recipe;
+    });
+  };
   initializeRecipes();
 });
