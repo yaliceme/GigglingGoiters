6a0167cd1c48485fb189188d3f1a2863b3fa0329
Peter Lollo <peteralollo@gmail.com>
2016-02-24 16:58:27 -0800

    Begin implementing API request for recipe details

--- foodZen/client/app/recipes/recipes.js
+++ foodZen/client/app/recipes/recipes.js
@@ -1,10 +1,8 @@
 angular.module('foodZen.recipes', [])
 .controller('RecipeController', function($scope, $http, Recipes, Ingredients){
   $scope.data = {};
-  $scope.singleView = {
-    view: false,
-    recipe: undefined
-  };
+  $scope.singleRecipe = {};
+  $scope.singleRecipe.view = false;
 
   var initializeRecipes = function(){
     Recipes.updateRecipes(Ingredients.ingredients)
@@ -15,11 +13,13 @@ angular.module('foodZen.recipes', [])
   };
 
   $scope.viewRecipe = function(id){
+    console.log('IDIDIDID', id);
     return $http({
       method: 'GET',
       url: '/api/recipes/ingredients/',
       data: {id: id}
     }).then(function( recipe ){
+      console.log('DID WE GET THE RECIPE BACK?', recipe);
       $scope.singleRecipe.view = true;
       $scope.singleRecipe.recipe = recipe;
     });
